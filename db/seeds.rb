5740f4066b2a7e47ea5ea33971e426c381f0faac
Prajyot Rajigare <prajyot.rajigare27@gmail.com>
2015-07-09 14:08:34 +0530

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(first_name: 'Prajyot', last_name: 'Rajigare', email: 'prajyot.rajigare27@gmail.com',  phone_no: '8983351062')
+User.create(first_name: 'Pooja', last_name: 'Rajigare', email: 'poojaalase@gmail.com',  phone_no: '8983351537')
+User.create(first_name: 'Prasmit', last_name: 'Rajigare', email: 'prasmit.rajigare@gmail.com',  phone_no: '8983351305')
